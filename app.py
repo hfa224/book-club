b3036618818ae03e9b943ef4548468da252953de
hfa224 <helenffionadams@gmail.com>
2024-09-02 21:11:51 +0100

    refs #3 tidy up and update login page styling

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['FREEZER_RELATIVE_URLS']= True
+app.config["FREEZER_RELATIVE_URLS"] = True
 
 
 @app.route("/")
@@ -15,49 +15,59 @@ def home():
     read_book_data()
     book_array = read_book_isbns()
 
-    current_book = max(book_array, key=lambda book: datetime.strptime(book.date, "%m-%Y").date())
+    current_book = max(
+        book_array, key=lambda book: datetime.strptime(book.date, "%m-%Y").date()
+    )
     book_array.remove(current_book)
-    return render_template('book_club.html', book_array=book_array, current_book=current_book)
+    return render_template(
+        "book_club.html", book_array=book_array, current_book=current_book
+    )
 
-@app.route('/book_club_about/')
+
+@app.route("/book_club_about/")
 def book_club_about():
-    return render_template('book_club_about.html')
+    return render_template("book_club_about.html")
+
 
 # This is a noddy way of making the site able to be made static
-@app.route('/book_club_wrapped/')
+@app.route("/book_club_wrapped/")
 def wrapped():
     # create a list of tuples
-    list = [("M_ENCRYPTED_PAYLOAD", book_club_wrapped("Max"), "max2024"), 
-            ("B_ENCRYPTED_PAYLOAD", book_club_wrapped("Beth"), "beth2024"), 
-            ("H_ENCRYPTED_PAYLOAD", book_club_wrapped("Helen"), "helen2024")]
+    list = [
+        ("M_ENCRYPTED_PAYLOAD", book_club_wrapped("Max"), "max2024"),
+        ("B_ENCRYPTED_PAYLOAD", book_club_wrapped("Beth"), "beth2024"),
+        ("H_ENCRYPTED_PAYLOAD", book_club_wrapped("Helen"), "helen2024"),
+    ]
     return encrypt(list)
+
 
 def book_club_wrapped(name):
     book_array = read_book_isbns()
     winner = []
     if name in who_has_the_most_genres(book_array):
         winner.append(who_has_the_most_genres(book_array))
-    
+
     picked_books = [x for x in book_array if x.picker.strip() == name.strip()]
     highest_rated_book = max(book_array, key=lambda book: book.rating[name])
-    highest_rated_picked_book = max(picked_books, key=lambda book: book.rating["Average"])
+    highest_rated_picked_book = max(
+        picked_books, key=lambda book: book.rating["Average"]
+    )
     genres = []
     for genre in map(lambda x: x.genre, book_array):
         for split_genre in genre.split("/"):
             genres.append(split_genre)
 
-    return render_template('book_club_wrapped.html', book_array=book_array, picked_books=picked_books, name=name, 
-                           highest_rated_book=highest_rated_book, highest_rated_picked_book=highest_rated_picked_book, genres=genres,
-                           winner=winner)
+    return render_template(
+        "book_club_wrapped.html",
+        book_array=book_array,
+        picked_books=picked_books,
+        name=name,
+        highest_rated_book=highest_rated_book,
+        highest_rated_picked_book=highest_rated_picked_book,
+        genres=genres,
+        winner=winner,
+    )
 
-@app.route('/<string:name>/book_club_stats/')
-def book_club_stats(name):
-    book_array = read_book_isbns()
-    picked_books = [x for x in book_array if x.picker.strip() == name.strip()]
-    highest_rated_book = max(book_array, key=lambda book: book.rating[name])
-    highest_rated_picked_book = max(picked_books, key=lambda book: book.rating["Average"])
-    return render_template('book_club_stats.html', book_array=book_array, picked_books=picked_books, name=name, 
-                           highest_rated_book=highest_rated_book, highest_rated_picked_book=highest_rated_picked_book)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- encrypt_flask_template.py
+++ encrypt_flask_template.py
@@ -5,14 +5,14 @@
 # Copyright (c) 2021 Samuel Plumppu
 
 try:
-	from Crypto import Random
-	from Crypto.Util.py3compat import bchr
-	from Crypto.Cipher import AES
-	from Crypto.Protocol.KDF import PBKDF2
-	from Crypto.Hash import SHA256	
+    from Crypto import Random
+    from Crypto.Util.py3compat import bchr
+    from Crypto.Cipher import AES
+    from Crypto.Protocol.KDF import PBKDF2
+    from Crypto.Hash import SHA256
 except:
-	print("install pycrypto: \"pip3 install pycrypto\"")
-	exit(1)
+    print('install pycrypto: "pip3 install pycrypto"')
+    exit(1)
 import os, sys
 from base64 import b64encode
 from getpass import getpass
@@ -20,41 +20,34 @@ import codecs
 
 
 def encrypt(tupleList):
-	# sanitize input
 
-	projectFolder = os.path.dirname(os.path.abspath(__file__))
-	with open(os.path.join(projectFolder, "templates/decryptTemplate.html")) as f:
-		templateHTML = f.read()
+    projectFolder = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(projectFolder, "templates/decryptTemplate.html")) as f:
+        templateHTML = f.read()
 
-	encryptedDocument = templateHTML
+    encryptedDocument = templateHTML
 
-	for tuple in tupleList:
+    for tuple in tupleList:
 
-		print(tuple[0])
+        print(tuple[0])
 
-		data = tuple[1].encode("utf8")
+        data = tuple[1].encode("utf8")
 
-		salt = Random.new().read(32)
-		key = PBKDF2(
-			tuple[2].encode('utf-8'), 
-			salt, 
-			count=100000,
-			dkLen=32, 
-			hmac_hash_module=SHA256
-		)
-		iv = Random.new().read(16)
+        salt = Random.new().read(32)
+        key = PBKDF2(
+            tuple[2].encode("utf-8"),
+            salt,
+            count=100000,
+            dkLen=32,
+            hmac_hash_module=SHA256,
+        )
+        iv = Random.new().read(16)
 
-		cipher = AES.new(key, AES.MODE_GCM, nonce=iv)
-		encrypted, tag = cipher.encrypt_and_digest(data)
-	
-		encryptedPl = f'"{b64encode(salt+iv+encrypted+tag).decode("utf-8")}"'
-		replaceString = "/*{{" + tuple[0] + "}}*/\"\""
-		print(replaceString)
-		encryptedDocument = encryptedDocument.replace(replaceString, encryptedPl)
+        cipher = AES.new(key, AES.MODE_GCM, nonce=iv)
+        encrypted, tag = cipher.encrypt_and_digest(data)
 
-	#filename, extension = os.path.splitext(inputfile)
-	#outputfile = filename + "-protected" + extension
-	#with codecs.open(outputfile, 'w','utf-8-sig') as f:
-	#	f.write(encryptedDocument)
-	#print("File saved to %s"%outputfile)
-	return encryptedDocument
+        encryptedPl = f'"{b64encode(salt+iv+encrypted+tag).decode("utf-8")}"'
+        replaceString = "/*{{" + tuple[0] + '}}*/""'
+        print(replaceString)
+        encryptedDocument = encryptedDocument.replace(replaceString, encryptedPl)
+    return encryptedDocument
