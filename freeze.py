eb9efd853f404cfcd9b2bdf40c0479648475d990
hfa224 <helenffionadams@gmail.com>
2025-08-25 16:02:09 +0100

    #25 update to use uv, also format, update github workflows

--- freeze.py
+++ freeze.py
@@ -1,11 +1,11 @@
+"""Uses flask freeze to generate static site files"""
 
-""" Uses flask freeze to generate static site files """
 from flask_frozen import Freezer
 from app import app
 
 freezer = Freezer(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Building website...")
     app.debug = False
     app.testing = True
