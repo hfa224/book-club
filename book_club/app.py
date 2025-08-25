5f30581332e329566d4a78e6d9f518172d853458
hfa224 <helenffionadams@gmail.com>
2025-08-25 16:18:49 +0100

    try to fix naming and project structure

---
+++ book_club/app.py
@@ -0,0 +1,89 @@
+"""Main flask app to serve book club site"""
+
+import os
+from datetime import datetime
+from flask import Flask, render_template
+from encrypt_flask_template import encrypt
+from read_data import (
+    read_book_isbns,
+    who_has_the_most_genres,
+    generate_next_pick_message,
+)
+
+app = Flask(__name__)
+
+app.config["FREEZER_RELATIVE_URLS"] = True
+
+
+@app.route("/")
+def home():
+    """Serve up the home page"""
+    book_array = read_book_isbns()
+
+    current_book = max(
+        book_array, key=lambda book: datetime.strptime(book["date"], "%m-%Y").date()
+    )
+    book_array.remove(current_book)
+
+    next_pick_message = generate_next_pick_message(current_book)
+    return render_template(
+        "book_club.html",
+        book_array=book_array,
+        current_book=current_book,
+        next_pick_message=next_pick_message,
+    )
+
+
+@app.route("/book_club_about/")
+def book_club_about():
+    """Serve up the about page"""
+    return render_template("book_club_about.html")
+
+
+@app.route("/book_club_wrapped/")
+def wrapped():
+    """Serve up the wrapped page"""
+    # create a list of tuples
+    members_list = [
+        ("M_ENCRYPTED_PAYLOAD", book_club_wrapped("Max"), "max2024"),
+        ("B_ENCRYPTED_PAYLOAD", book_club_wrapped("Beth"), "beth2024"),
+        ("H_ENCRYPTED_PAYLOAD", book_club_wrapped("Helen"), "helen2024"),
+    ]
+    return encrypt(members_list)
+
+
+def book_club_wrapped(name):
+    """Generate book club wrapped page from template"""
+    book_array = read_book_isbns()
+    winner = []
+    if name in who_has_the_most_genres(book_array):
+        winner.append(who_has_the_most_genres(book_array))
+
+    picked_books = [x for x in book_array if x["picker"].strip() == name.strip()]
+    # we need to filter dnfs out here
+    highest_rated_book = max(
+        book_array,
+        key=lambda book: book["rating"][name] if book["rating"][name] != "dnf" else "0",
+    )
+    highest_rated_picked_book = max(
+        picked_books, key=lambda book: book["rating"]["Average"]
+    )
+    genres = []
+    for genre in map(lambda x: x["genre"], book_array):
+        for split_genre in genre.split("/"):
+            genres.append(split_genre)
+
+    return render_template(
+        "book_club_wrapped.html",
+        book_array=book_array,
+        picked_books=picked_books,
+        name=name,
+        highest_rated_book=highest_rated_book,
+        highest_rated_picked_book=highest_rated_picked_book,
+        genres=genres,
+        winner=winner,
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
---
+++ book_club/encrypt_flask_template.py
@@ -0,0 +1,56 @@
+"""Contains encryption logic for generating wrapped pages with login"""
+#!/usr/bin/python3
+# Based on PageCrypt
+# Original code is
+# Copyright (c) 2021 Maximillian Laumeister
+# Copyright (c) 2021 Samuel Plumppu
+
+import os
+import sys
+from base64 import b64encode
+
+try:
+    from Crypto import Random
+    from Crypto.Cipher import AES
+    from Crypto.Protocol.KDF import PBKDF2
+    from Crypto.Hash import SHA256
+except ImportError:
+    print('install pycrypto: "pip3 install pycrypto"')
+    sys.exit(1)
+
+
+def encrypt(members_list):
+    """Encrypt the page generated for each member and insert it in the decrypt template HTML
+    based on the key given for each member"""
+
+    project_folder = os.path.dirname(os.path.abspath(__file__))
+    with open(
+        file=os.path.join(project_folder, "templates/decryptTemplate.html"),
+        encoding="utf8",
+    ) as f:
+        template_html = f.read()
+
+    encrypted_doc = template_html
+
+    for member_info_tuple in members_list:
+        print(member_info_tuple[0])
+
+        data = member_info_tuple[1].encode("utf8")
+
+        salt = Random.new().read(32)
+        key = PBKDF2(
+            member_info_tuple[2].encode("utf-8"),
+            salt,
+            count=100000,
+            dkLen=32,
+            hmac_hash_module=SHA256,
+        )
+        iv = Random.new().read(16)
+
+        cipher = AES.new(key, AES.MODE_GCM, nonce=iv)
+        encrypted, tag = cipher.encrypt_and_digest(data)
+
+        encrypted_pl = f'"{b64encode(salt + iv + encrypted + tag).decode("utf-8")}"'
+        replace_string = "/*{{" + member_info_tuple[0] + '}}*/""'
+        encrypted_doc = encrypted_doc.replace(replace_string, encrypted_pl)
+    return encrypted_doc
